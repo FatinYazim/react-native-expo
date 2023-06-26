ca706ad8e5d32d8a81763fef6db82c03c9fb7a81
FatinYazim <123831827+FatinYazim@users.noreply.github.com>
2023-06-27 03:27:33 +0800

    Add files via upload

---
+++ App.js
@@ -0,0 +1,14 @@
+
+import CameraScreen from './screens/CameraScreen';
+import ImagePickerScreen from './screens/ImagePickerScreen';
+import ImagePicker from './screens/ImagePickerScreen';
+import WebViewScreen from './screens/WebViewScreen';
+
+ function App() {
+  return (
+<WebViewScreen/>
+  );
+
+}
+
+export default App;
